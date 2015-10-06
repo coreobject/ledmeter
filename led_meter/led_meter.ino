a50ad1ba25a00920abc40d810a156a65ba9eee00
mixania <mbravermna96@gmail.com>
2015-10-06 14:48:00 -0400

    many, many pins

--- led_meter/led_meter.ino
+++ led_meter/led_meter.ino
@@ -1,10 +1,12 @@
 int ledPin1 = 11;
 int ledPin2 = 12;
 int ledPin3 = 13;
+
+int pins[] = {13,12,11,10,9,8};
 void setup() {
-  pinMode(ledPin1,OUTPUT);
-  pinMode(ledPin2,OUTPUT);
-  pinMode(ledPin3,OUTPUT);
+  for (int i = 0; i < 6; i++) {
+    pinMode(pins[i], OUTPUT);
+  }
 }
 
 void loop() {
@@ -13,22 +15,14 @@ void loop() {
   // The analog pin reads from 0 to 1023.
   // This commands maps those numbers from 4 to 0
   // So 0 becomes 4 and 1023 becomes 0
-  int mappedSensorValue = map( sensorValue, 0, 1023, 4, 0);
+  int mappedSensorValue = map( sensorValue, 0, 1023, 6, 0);
   //Here we test the mappedsensorvalue. If it is greater then 1 we turn
   // on the LED on Pin 2 and then see if it is greater then 2.
-    if (mappedSensorValue > 1) {
-      digitalWrite ( ledPin3, HIGH);
-      if (mappedSensorValue > 2) {
-          digitalWrite ( ledPin2, HIGH);
-          if (mappedSensorValue > 3) {
-              digitalWrite ( ledPin1, HIGH);
-          }else{//If it is not greater we start turning LEDs off.
-              digitalWrite ( ledPin1, LOW);
-          }
-      }else{
-          digitalWrite ( ledPin2, LOW);
-      }
-    }else{
-      digitalWrite ( ledPin3, LOW);
+  for (int i = 0; i < 6; i++) {
+    if (i < mappedSensorValue) {
+      digitalWrite(pins[i], HIGH);
+    } else {
+      digitalWrite(pins[i], LOW);
     }
+  }
 }
